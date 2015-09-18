8e6ee8632796a2fa20dd038ae1902597228d9b00
nick_artin <nickartin13@gmail.com>
2015-09-18 10:47:12 +0200

    Remove some redundant

--- linregpackage/R/linreg.R
+++ linregpackage/R/linreg.R
@@ -1,49 +1,49 @@
 linreg <- function (formula, data) {
   stopifnot((class(formula)=="formula") && (class(data)=="data.frame"))
-  
-  
+
+
   # uses model.matrix and all.vars on data and formula, and then calculates the regressions coefficients.
-  # to test: 
+  # to test:
   # formula <- eruptions ~ waiting
   # data <- faithful
   # attach(faithful)
   X <- model.matrix(formula, data)
   X
-  y_namn <- all.vars(formula, max.names=1L) 
+  y_namn <- all.vars(formula, max.names=1L)
   y_namn
   y <- data[[y_namn]]
   y
   del_a <- t(X) %*% X
-  del_b <- solve(del_a)  # solve(A) 	Inverse of A where A is a square matrix. 
-  
+  del_b <- solve(del_a)  # solve(A) 	Inverse of A where A is a square matrix.
+
   reg_coef <- del_b %*% t(X) %*% y  # "Regressions coefficients"
   reg_coef
-  
+
   fitted_values <- X %*% reg_coef  # "The fitted values"
   fitted_values
-  
+
   resi <- as.vector(y - fitted_values) # "The residuals"
   resi
-  
+
   n <- length(y)
   p <- length(data)
   deg_free <- n - p         # "The degrees of freedom"
   deg_free
-  
+
   res_var <- as.vector((t(resi) %*% resi) / deg_free)  # "The residual variance"
   res_var
- 
-  var_reg_coef <- res_var * del_b  # "The variance of the regression coefficients"  
+
+  var_reg_coef <- res_var * del_b  # "The variance of the regression coefficients"
   var_reg_coef
-  
+
   t_each_coef <- reg_coef %/% sqrtm(var_reg_coef)    # OBS: something wrong here!
   t_each_coef
-  
-  p_values <- pt(reg_coef, df = deg_free)   # "p-values for the regressions coefficients"  
-  
+
+  p_values <- pt(reg_coef, df = deg_free)   # "p-values for the regressions coefficients"
+
   #the code below is there temporarily to get the correct output value.
   res_temp <- lm(formula, data)
-  return(res_temp) 
-  
-  
+  return(res_temp)
+
+
 }

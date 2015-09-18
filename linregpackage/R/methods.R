18971bdd07fd8b7cbb9a4276adad569fe0b4a2d3
nick_artin <nickartin13@gmail.com>
2015-09-18 12:49:48 +0200

    revert the change, wait for Camelia update

--- linregpackage/R/methods.R
+++ linregpackage/R/methods.R
@@ -1,15 +1,3 @@
 #tries to do the plot operations
 
-setMethod('as.data.frame', 'linreg', function(x){
-	df = data.frame(fitted_values=x$fitted_values,residuals=x$residuals)
-	return(df)
-})
-
-setMethod('plot','linreg',function(x, ...){
-	x = as.data.frame(x)
-	ggplot(data=x) +
-	geom_point(shape=1, size=10, aes(x=fitted_values, y=residuals)) + ggtitle("Residuals vs Fitted")
-})
-
-r = linreg(Sepal.Length ~ Sepal.Width, iris)
-plot(r)
+ggplot(mod_object) + geom_point(shape=1, size=10, aes(x=mod_object$fitted, y=mod_object$residual)) + ggtitle("Residuals vs Fitted")

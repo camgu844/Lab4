22b628f755f5f4e78ae7367e000e8013a7557b6a
Camilla Skoglund <camgu844@student.liu.se>
2015-09-18 12:40:17 +0200

    added code

--- linregpackage/R/methods.R
+++ linregpackage/R/methods.R
@@ -1,3 +1,18 @@
 #tries to do the plot operations
 
-ggplot(mod_object) + geom_point(shape=1, size=10, aes(x=mod_object$fitted, y=mod_object$residual)) + ggtitle("Residuals vs Fitted")
+data(iris)
+mod_object <- lm(Petal.Length~Species, data=iris)
+print(mod_object)
+
+as.data.frame.linreg <- function(x) {
+  df <- data.frame(fitted=x$fitted_values, residuals=x$resi)
+  return(df)
+}
+
+plot.linreg <- function(y, ...) {
+  y <- as.data.frame(y)
+  ggplot(y) + geom_point(shape=1, size=10, aes(x=fitted, y=residuals)) + ggtitle("Residuals vs Fitted")
+}
+
+r <- linreg(Petal.Length~Species, data=iris)
+plot.linreg(r)

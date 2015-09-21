26b1a62633cd21f971c28947a498861c78cb9604
Camilla Skoglund <camgu844@student.liu.se>
2015-09-21 09:00:48 +0200

    added new tests

--- linregpackage/tests/testthat/test_methods.R
+++ linregpackage/tests/testthat/test_methods.R
@@ -4,8 +4,9 @@ context("tests the methods coefficients, resid and pred")
 formula <- eruptions ~ waiting
 data <- faithful
 
-test_that("return values are correct", {
-  expect_that(class(coeff), equals("integer"|"numeric"))
-  # not finished expect_that()
+test_that("return values are of correct class", {
+  expect_that(mode(coeff), equals("numeric"))
+  expect_that(mode(resid), equals("numeric"))
+  expect_that(mode(coeff), equals("numeric"))
 })
 
